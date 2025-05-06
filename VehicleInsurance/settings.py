48ce1a499979633df41573a5a5888b66153dcc5a
Szymqn <koodziejskisz@outlook.com>
2025-05-06 22:20:56 +0200

    fix: Django CI env variable

--- VehicleInsurance/settings.py
+++ VehicleInsurance/settings.py
@@ -13,6 +13,7 @@ import os
 
 from pathlib import Path
 from dotenv import load_dotenv
+from decouple import config
 
 load_dotenv()
 
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

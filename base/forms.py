d9ab82227c3aa778ebd6e79965ad74721de1a828
Szymqn <koodziejskisz@outlook.com>
2025-05-06 22:28:23 +0200

    Revert "feat: add invalid data handler & default dates"
    This reverts commit a384bf0174a55585a5614e9810671fc0b0870c42.

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
 
 
 class InsuranceForm(forms.Form):
@@ -26,10 +24,8 @@ class InsuranceForm(forms.Form):
     vehicle_price = forms.DecimalField(max_digits=10, decimal_places=2, label='Vehicle Price')
     mileage_per_year = forms.IntegerField(label='Mileage per Year')
 
-    start_date = forms.DateField(widget=forms.SelectDateWidget(), label='Start Date',
-                                 initial=(datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d"))
-    end_date = forms.DateField(widget=forms.SelectDateWidget(), label='End Date',
-                               initial=(datetime.today() + relativedelta(days=1, years=1)).strftime("%Y-%m-%d"))
+    start_date = forms.DateField(widget=forms.SelectDateWidget(), label='Start Date')
+    end_date = forms.DateField(widget=forms.SelectDateWidget(), label='End Date')
     previous_insurance = forms.BooleanField(required=False, initial=True, label='Previous Insurance')
     previous_accidents = forms.IntegerField(required=True, initial=0, label='Previous Accidents')
     country = forms.CharField(max_length=100, label='Country')
